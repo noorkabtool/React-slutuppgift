3cd8a7a19a88d2e70677ae6e3da17811d28cb910
noor kabtool <noor.nk@gmail.com>
2018-04-29 00:42:37 +0200

    Fetch articles from newsApi and render them in the articlesList component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import TextField from 'material-ui/TextField';
+
+import ArticlesList from './components/articles'
+
 import logo from './logo.svg';
 import './App.css';
+import newsapi from './api/newsApi';
 
 class App extends Component {
+  state = {
+    articles: [],
+  }
+
+  componentDidMount() {
+    this.getArticles();
+  }
+
+  search(e, newValue) {
+    this.getArticles(newValue);
+  }
+
+  getArticles(query) {
+    newsapi.v2.everything({
+      sources: 'the-verge',
+      q: query,
+      language: 'en',
+    }).then(response => {
+      this.setState({articles: response.articles});
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +39,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MuiThemeProvider>
+          <TextField
+            hintText="Search"
+            onChange={(e, newValue) => this.search(e, newValue)}
+          />
+        </MuiThemeProvider>
+        <ArticlesList articles={this.state.articles}/>
+        <footer> Powered by <a href="https://newsapi.org/"> NewsApi </a></footer>
       </div>
     );
   }
