9d53e714bac660b595580bd561bb54670df51044
noor kabtool <noor.nk@gmail.com>
2018-04-29 00:53:35 +0200

    Fix the logo style

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <br/>
           <img src={VergeName} className="App-logo" alt="logo" />
           
         </header>
